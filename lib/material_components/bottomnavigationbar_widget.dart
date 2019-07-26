bb85c55a9ecbd253cce5907fdb503fd1244bd8a7
ZhangXinmin528 <zhangxinmin528@sina.com>
2019-07-26 17:11:33 +0800

    add material component

---
+++ lib/material_components/bottomnavigationbar_widget.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String _title =
+      'Material components widget : BottomNavigationBar';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyStateFulWidgetState();
+  }
+}
+
+class _MyStateFulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0 : Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1 : Music',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2 : Video',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material components widget : BottomNavigationBar'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_note), title: Text('Music')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ondemand_video), title: Text('Video')),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
