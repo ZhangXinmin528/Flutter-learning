5770553d8be2bb59cae4ffd48ab60f9242afc213
ZhangXinmin528 <zhangxinmin528@sina.com>
2019-12-30 16:44:05 +0800

    finish networking usage example

--- lib/guide/networking/FetchDataDemo.dart
+++ lib/guide/networking/FetchDataDemo.dart
@@ -1,15 +1,85 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main(){
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+//获取网络数据
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
-
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final appTitle = 'Fetch Data Example';
+  Future<Post> post;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    post = fetchPost();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: appTitle,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text(snapshot.error);
+              }
+              return CircularProgressIndicator();
+            },
+            future: post,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+    );
+  }
+}
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
 }
