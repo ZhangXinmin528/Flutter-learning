92ad2d4ce623617c1ae147bce39361719b468429
ZhangXinmin528 <zhangxinmin528@sina.com>
2020-01-10 16:06:12 +0800

    add flutter_timer with the 'Bloc' pattern

---
+++ example/bloc/flutter_timer/flutter_timer/lib/ticker.dart
@@ -0,0 +1,12 @@
+//The ticker will be our data source for the timer application.
+// It will expose a stream of ticks which we can subscribe and react to.
+class Ticker {
+  Stream<int> tick({int ticks}) {
+    return Stream.periodic(
+      Duration(seconds: 1),
+      (x) {
+        return ticks - x - 1;
+      },
+    ).take(ticks);
+  }
+}
